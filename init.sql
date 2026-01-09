9171b9f298278688e6e01118715e7ebabf24bb17
Maxim Merkurev <cabamax@yandex.ru>
2026-01-10 02:00:49 +1000

    Fix migrations init.sql

--- init.sql
+++ init.sql
@@ -83,67 +83,54 @@ values
      'ivanov.ii@example.com', '+79990000001',
      (select id from positions where code = 'OFFICE_HEAD'),
      'ACTIVE', date '2023-10-10', null),
-
     (2, 1, 'EMP-000002', 'Петров Пётр Сергеевич',
      'petrov.ps@example.com', '+79990000002',
      (select id from positions where code = 'CLIENT_MANAGER'),
      'ACTIVE', date '2024-02-01', null),
-
     (3, 1, 'EMP-000003', 'Сидорова Анна Владимировна',
      'sidorova.av@example.com', '+79990000003',
      (select id from positions where code = 'VIP_CLIENT_MANAGER'),
      'ACTIVE', date '2024-03-12', null),
-
     (4, 1, 'EMP-000004', 'Кузнецов Дмитрий Олегович',
      'kuznetsov.do@example.com', '+79990000004',
      (select id from positions where code = 'LEGAL_ENTITY_MANAGER'),
      'ACTIVE', date '2024-04-05', null),
-
     (5, 1, 'EMP-000005', 'Морозова Елена Николаевна',
      'morozova.en@example.com', '+79990000005',
      (select id from positions where code = 'CASHIER'),
      'ACTIVE', date '2024-01-15', null),
-
     (6, 1, 'EMP-000006', 'Смирнов Артём Андреевич',
      'smirnov.aa@example.com', '+79990000006',
      (select id from positions where code = 'CASHIER'),
      'ACTIVE', date '2024-06-01', null),
-
     (7, 1, 'EMP-000007', 'Орлова Мария Игоревна',
      'orlova.mi@example.com', '+79990000007',
      (select id from positions where code = 'CLIENT_MANAGER'),
      'ACTIVE', date '2024-07-18', null),
-
     (8, 2, 'EMP-000008', 'Васильев Алексей Николаевич',
      'vasilev.an@example.com', '+79990000008',
      (select id from positions where code = 'OFFICE_HEAD'),
      'ACTIVE', date '2022-09-01', null),
-
     (9, 2, 'EMP-000009', 'Фёдорова Ольга Сергеевна',
      'fedorova.os@example.com', '+79990000009',
      (select id from positions where code = 'VIP_CLIENT_MANAGER'),
      'ACTIVE', date '2023-11-20', null),
-
     (10, 2, 'EMP-000010', 'Никитин Павел Дмитриевич',
      'nikitin.pd@example.com', '+79990000010',
      (select id from positions where code = 'LEGAL_ENTITY_MANAGER'),
      'ACTIVE', date '2024-02-10', null),
-
     (11, 2, 'EMP-000011', 'Захарова Ирина Викторовна',
      'zaharova.iv@example.com', '+79990000011',
      (select id from positions where code = 'CLIENT_MANAGER'),
      'ACTIVE', date '2024-03-25', null),
-
     (12, 2, 'EMP-000012', 'Громов Максим Евгеньевич',
      'gromov.me@example.com', '+79990000012',
      (select id from positions where code = 'CASHIER'),
      'ACTIVE', date '2024-01-09', null),
-
     (13, 2, 'EMP-000013', 'Белова Татьяна Андреевна',
      'belova.ta@example.com', '+79990000013',
      (select id from positions where code = 'CASHIER'),
      'FIRED', date '2023-12-05', date '2025-01-31'),
-
     (14, 2, 'EMP-000014', 'Комаров Илья Романович',
      'komarov.ir@example.com', '+79990000014',
      (select id from positions where code = 'CLIENT_MANAGER'),
